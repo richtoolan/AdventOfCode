41065fb20acbf63408202184bcbffa079c310241
richard.toolan <richard.toolan@travelportdigital.com>
2019-12-06 11:48:55 +0000

    Updating for day 2

--- src/main/java/days/AdventOfCodeTwo.java
+++ src/main/java/days/AdventOfCodeTwo.java
@@ -4,59 +4,84 @@ import model.Opcode;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class AdventOfCodeTwo {
-    public List<Integer> runProgram(List<Integer> inputs, Integer currentIndex) {
-        if (inputs == null || inputs.size() == 0 && currentIndex <= inputs.size()) {
+
+    private static final int NEXT_INSTRUCTION_OFFSET = 4;
+    private static final int RESULT_PARAMETER_OFFSET = 3;
+
+    public static void main(String... args) {
+        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+
+        Boolean winner = false;
+        int verb = 0;
+        int noun = 0;
+        while(!winner) {
+
+            Map<Integer, Integer> initialState = Map.of(
+              1, verb,
+              2, noun
+            );
+
+            List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 5, 23, 1, 23, 6, 27, 2, 9, 27, 31, 1, 5, 31, 35, 1, 35, 10, 39, 1, 39, 10, 43, 2, 43, 9, 47, 1, 6, 47, 51, 2, 51, 6, 55, 1, 5, 55, 59, 2, 59, 10, 63, 1, 9, 63, 67, 1, 9, 67, 71, 2, 71, 6, 75, 1, 5, 75, 79, 1, 5, 79, 83, 1, 9, 83, 87, 2, 87, 10, 91, 2, 10, 91, 95, 1, 95, 9, 99, 2, 99, 9, 103, 2, 10, 103, 107, 2, 9, 107, 111, 1, 111, 5, 115, 1, 115, 2, 119, 1, 119, 6, 0, 99, 2, 0, 14, 0), initialState);
+
+            if (output.get(0) == 19690720) {
+                System.out.println("We have a winner!" + (100 * initialState.get(1) + initialState.get(2)));
+                winner = true;
+            } else if(verb == 99) {
+                verb = 0;
+                noun ++;
+            } else {
+                verb ++;
+            }
+        }
+
+    }
+
+    public List<Integer> executeProgram(List<Integer> inputs, Map<Integer, Integer> initialState) {
+        return runProgram(inputs, 0, initialState);
+    }
+
+    private List<Integer> runProgram(List<Integer> inputs, Integer instructionPointer, Map<Integer, Integer> initialState) {
+        if (inputs == null || inputs.size() == 0 && instructionPointer <= inputs.size()) {
             return inputs;
         }
 
-        Opcode opcode = getNextOpCode(inputs.subList(currentIndex, inputs.size()));
+        Opcode opcode = getNextOpCode(inputs.subList(instructionPointer, inputs.size()));
 
-        if(currentIndex == 0) {
-            inputs.set(1, 12);
-            inputs.set(2, 2);
+        if(initialState != null && !initialState.isEmpty() && instructionPointer == 0 ) {
+            initialState.keySet().forEach(key -> inputs.set(key, initialState.get(key)));
         }
 
-        int leftOperandIndex;
-        int rightOperandIndex;
+        Parameters parameters;
         int resultIndex;
-
-        int leftOperand;
-        int rightOperand;
-
         int result;
 
         switch (opcode) {
+            case ABORT_OPCODE:
+                break;
             case ADD_OPCODE:
-                leftOperandIndex = inputs.get(currentIndex +1);
-                rightOperandIndex = inputs.get(currentIndex +2);
-                resultIndex = inputs.get(currentIndex + 3);
+                parameters = new Parameters(inputs, instructionPointer).invoke();
 
-                leftOperand = inputs.get(leftOperandIndex);
-                rightOperand = inputs.get(rightOperandIndex);
-
-                result = leftOperand + rightOperand;
+                result = parameters.getFirstOperand() + parameters.getSecondOperand();
+                resultIndex = inputs.get(instructionPointer + RESULT_PARAMETER_OFFSET);
 
                 inputs.set(resultIndex, result);
 
-                runProgram(inputs, currentIndex + 4);
+                runProgram(inputs, instructionPointer + NEXT_INSTRUCTION_OFFSET, initialState);
                 break;
             case MULTIPLY_OPCODE:
-                leftOperandIndex = inputs.get(currentIndex +1);
-                rightOperandIndex = inputs.get(currentIndex +2);
-                resultIndex = inputs.get(currentIndex + 3);
+                parameters = new Parameters(inputs, instructionPointer).invoke();
 
-                leftOperand = inputs.get(leftOperandIndex);
-                rightOperand = inputs.get(rightOperandIndex);
+                resultIndex = inputs.get(instructionPointer + RESULT_PARAMETER_OFFSET);
 
-                result = leftOperand * rightOperand;
+
+                result = parameters.getFirstOperand() * parameters.getSecondOperand();
 
                 inputs.set(resultIndex, result);
 
-                runProgram(inputs, currentIndex + 4);
-                break;
-            case ABORT_OPCODE:
+                runProgram(inputs, instructionPointer + NEXT_INSTRUCTION_OFFSET, initialState);
                 break;
         }
 
@@ -65,5 +90,31 @@ public class AdventOfCodeTwo {
 
     public Opcode getNextOpCode(List<Integer> slicedInputs) {
         return Arrays.stream(Opcode.values()).filter(opcode -> opcode.getId() == slicedInputs.get(0)).findFirst().get();
+    }
+
+    private class Parameters {
+        private List<Integer> inputs;
+        private Integer currentIndex;
+        private int leftOperandIndex;
+        private int rightOperandIndex;
+
+        public Parameters(List<Integer> inputs, Integer currentIndex) {
+            this.inputs = inputs;
+            this.currentIndex = currentIndex;
+        }
+
+        public int getFirstOperand() {
+            return inputs.get(leftOperandIndex);
+        }
+
+        public int getSecondOperand() {
+            return inputs.get(rightOperandIndex);
+        }
+
+        public Parameters invoke() {
+            leftOperandIndex = inputs.get(currentIndex +1);
+            rightOperandIndex = inputs.get(currentIndex +2);
+            return this;
+        }
     }
 }
--- src/test/java/AdventOfCodeTwoTest.java
+++ src/test/java/AdventOfCodeTwoTest.java
@@ -3,55 +3,76 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdventOfCodeTwoTest {
-    @Test
-    void shouldRunProgram() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(1,9,10,3,2,3,11,0,99,30,40,50), 0);
+  @Test
+  void shouldRunProgram() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50), null);
 
-        assertEquals(Arrays.asList(3500,9,10,70,2,3,11,0,99,30,40,50), output);
-    }
+    assertEquals(Arrays.asList(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50), output);
+  }
 
-    @Test
-    void shouldRunProgram2() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(1,0,0,0,99), 0);
+  @Test
+  void shouldRunProgram2() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 0, 0, 0, 99), null);
 
-        assertEquals(Arrays.asList(2,0,0,0,99), output);
-    }
+    assertEquals(Arrays.asList(2, 0, 0, 0, 99), output);
+  }
 
-    @Test
-    void shouldRunProgram3() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(2,3,0,3,99), 0);
+  @Test
+  void shouldRunProgram3() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(2, 3, 0, 3, 99), null);
 
-        assertEquals(Arrays.asList(2,3,0,6,99), output);
-    }
+    assertEquals(Arrays.asList(2, 3, 0, 6, 99), output);
+  }
 
-    @Test
-    void shouldRunProgram4() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(2,4,4,5,99,0), 0);
+  @Test
+  void shouldRunProgram4() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(2, 4, 4, 5, 99, 0), null);
 
-        assertEquals(Arrays.asList(2,4,4,5,99,9801), output);
-    }
+    assertEquals(Arrays.asList(2, 4, 4, 5, 99, 9801), output);
+  }
 
-    @Test
-    void shouldRunProgram5() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(1,1,1,4,99,5,6,0,99), 0);
+  @Test
+  void shouldRunProgram5() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 1, 1, 4, 99, 5, 6, 0, 99), null);
 
-        assertEquals(Arrays.asList(30,1,1,4,2,5,6,0,99), output);
-    }
+    assertEquals(Arrays.asList(30, 1, 1, 4, 2, 5, 6, 0, 99), output);
+  }
 
-    @Test
-    void shouldRunProgram6() {
-        AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
-        List<Integer> output = adventOfCodeTwo.runProgram(Arrays.asList(1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,6,27,2,9,27,31,1,5,31,35,1,35,10,39,1,39,10,43,2,43,9,47,1,6,47,51,2,51,6,55,1,5,55,59,2,59,10,63,1,9,63,67,1,9,67,71,2,71,6,75,1,5,75,79,1,5,79,83,1,9,83,87,2,87,10,91,2,10,91,95,1,95,9,99,2,99,9,103,2,10,103,107,2,9,107,111,1,111,5,115,1,115,2,119,1,119,6,0,99,2,0,14,0), 0);
+  @Test
+  void shouldRunProgram6() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
 
-        assertEquals(Arrays.asList(30,1,1,4,2,5,6,0,99), output);
-    }
+    Map<Integer, Integer> intialState = Map.of(
+      1, 12,
+      2, 2
+    );
+
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 5, 23, 1, 23, 6, 27, 2, 9, 27, 31, 1, 5, 31, 35, 1, 35, 10, 39, 1, 39, 10, 43, 2, 43, 9, 47, 1, 6, 47, 51, 2, 51, 6, 55, 1, 5, 55, 59, 2, 59, 10, 63, 1, 9, 63, 67, 1, 9, 67, 71, 2, 71, 6, 75, 1, 5, 75, 79, 1, 5, 79, 83, 1, 9, 83, 87, 2, 87, 10, 91, 2, 10, 91, 95, 1, 95, 9, 99, 2, 99, 9, 103, 2, 10, 103, 107, 2, 9, 107, 111, 1, 111, 5, 115, 1, 115, 2, 119, 1, 119, 6, 0, 99, 2, 0, 14, 0), intialState);
+
+    assertEquals(Arrays.asList(3516593, 12, 2, 2, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 36, 1, 19, 5, 37, 1, 23, 6, 39, 2, 9, 27, 117, 1, 5, 31, 118, 1, 35, 10, 122, 1, 39, 10, 126, 2, 43, 9, 378, 1, 6, 47, 380, 2, 51, 6, 760, 1, 5, 55, 761, 2, 59, 10, 3044, 1, 9, 63, 3047, 1, 9, 67, 3050, 2, 71, 6, 6100, 1, 5, 75, 6101, 1, 5, 79, 6102, 1, 9, 83, 6105, 2, 87, 10, 24420, 2, 10, 91, 97680, 1, 95, 9, 97683, 2, 99, 9, 293049, 2, 10, 103, 1172196, 2, 9, 107, 3516588, 1, 111, 5, 3516589, 1, 115, 2, 3516591, 1, 119, 6, 0, 99, 2, 0, 14, 0), output);
+  }
+
+  @Test
+  void shouldRunProgram7() {
+    AdventOfCodeTwo adventOfCodeTwo = new AdventOfCodeTwo();
+
+    Map<Integer, Integer> intialState = Map.of(
+      1, 12,
+      2, 2
+    );
+
+    List<Integer> output = adventOfCodeTwo.executeProgram(Arrays.asList(1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 19, 1, 19, 5, 23, 1, 23, 6, 27, 2, 9, 27, 31, 1, 5, 31, 35, 1, 35, 10, 39, 1, 39, 10, 43, 2, 43, 9, 47, 1, 6, 47, 51, 2, 51, 6, 55, 1, 5, 55, 59, 2, 59, 10, 63, 1, 9, 63, 67, 1, 9, 67, 71, 2, 71, 6, 75, 1, 5, 75, 79, 1, 5, 79, 83, 1, 9, 83, 87, 2, 87, 10, 91, 2, 10, 91, 95, 1, 95, 9, 99, 2, 99, 9, 103, 2, 10, 103, 107, 2, 9, 107, 111, 1, 111, 5, 115, 1, 115, 2, 119, 1, 119, 6, 0, 99, 2, 0, 14, 0), intialState);
+
+    assertEquals(Arrays.asList(3516593, 12, 2, 2, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 9, 1, 36, 1, 19, 5, 37, 1, 23, 6, 39, 2, 9, 27, 117, 1, 5, 31, 118, 1, 35, 10, 122, 1, 39, 10, 126, 2, 43, 9, 378, 1, 6, 47, 380, 2, 51, 6, 760, 1, 5, 55, 761, 2, 59, 10, 3044, 1, 9, 63, 3047, 1, 9, 67, 3050, 2, 71, 6, 6100, 1, 5, 75, 6101, 1, 5, 79, 6102, 1, 9, 83, 6105, 2, 87, 10, 24420, 2, 10, 91, 97680, 1, 95, 9, 97683, 2, 99, 9, 293049, 2, 10, 103, 1172196, 2, 9, 107, 3516588, 1, 111, 5, 3516589, 1, 115, 2, 3516591, 1, 119, 6, 0, 99, 2, 0, 14, 0), output);
+  }
 }
